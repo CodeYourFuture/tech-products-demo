002766be45b6bd8ac7233e85f318a5427012f494
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-07-23 09:07:38 +0100

    Add JSDoc to migration template

--- db/migration-template.js
+++ db/migration-template.js
@@ -1,6 +1,33 @@
 /* eslint-disable no-unused-vars */
-exports.shorthands = undefined;
 
+/**
+ * Importing library types for better type hints.
+ *
+ * See {@link https://salsita.github.io/node-pg-migrate/#/migrations?id=defining-migrations the docs}.
+ *
+ * @typedef {import("node-pg-migrate").MigrationBuilder} MigrationBuilder
+ * @typedef {import("node-pg-migrate").ColumnDefinitions} ColumnDefinitions
+ */
+
+/**
+ * Defines changes to be made to the database schema to accommodate new functionality.
+ *
+ * @param {MigrationBuilder} pgm
+ * @returns {void | Promise<void>}
+ */
 exports.up = (pgm) => {};
 
+/**
+ * Reverses the "up" migration to return the database to its initial state.
+ *
+ * @param {MigrationBuilder} pgm
+ * @returns {void | Promise<void>}
+ */
 exports.down = (pgm) => {};
+
+/**
+ * Create new shorthand column definitions for this and any future migrations.
+ *
+ * @type {ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
