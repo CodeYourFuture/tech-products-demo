21dc70457c79b83cd36a74a9c9dcd26d9e636143
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-07-23 09:25:35 +0100

    Add some basic global styles

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import "./index.scss";
 
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
