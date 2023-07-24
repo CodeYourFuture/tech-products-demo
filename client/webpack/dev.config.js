615f6f5d2ac385615d6784794de619750c2155d5
Jonathan Sharpe <jonathan@codeyourfuture.io>
2023-07-24 11:15:42 +0000

    Get dev server working in Codespace

--- client/webpack/dev.config.js
+++ client/webpack/dev.config.js
@@ -2,13 +2,22 @@ const { merge } = require("webpack-merge");
 
 const common = require("./common.config");
 
+const codespace = process.env.CODESPACE_NAME;
+const port = 4201;
+
 module.exports = merge(common, {
 	devtool: "inline-source-map",
 	devServer: {
+		allowedHosts: codespace && [`${codespace}-${port}.preview.app.github.dev`],
+		client: {
+			webSocketURL: {
+				port: codespace && 443,
+			},
+		},
 		historyApiFallback: {
 			disableDotRule: true,
 		},
-		port: 4201,
+		port: port,
 		proxy: [
 			{
 				context: ["/api", "/docs", "/health"],
