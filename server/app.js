2e4e9d508db2f3a9e8daa21b628bfc4c346830b3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-07-26 10:41:09 +0100

    Move session store creation into db file
    Allows access to the pool without exposing it.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,9 @@
-import pgSession from "connect-pg-simple";
 import express from "express";
-import session, { MemoryStore } from "express-session";
+import session from "express-session";
 import passport from "passport";
 
 import apiRouter from "./api";
-import pool from "./db";
+import { getSessionStore } from "./db";
 import docsRouter from "./docs";
 import config from "./utils/config";
 import {
@@ -15,24 +14,13 @@ import {
 	logErrors,
 } from "./utils/middleware";
 
-function getStore(store) {
-	switch (store) {
-		case "memory":
-			return new MemoryStore();
-		case "postgres":
-			return new (pgSession(session))({ pool, tableName: "sessions" });
-		default:
-			throw new Error(`unknown store type: ${store}`);
-	}
-}
-
 const apiRoot = "/api";
 const sessionConfig = {
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
 	secret: config.sessionSecret,
-	store: getStore(config.sessionStore),
+	store: getSessionStore(),
 };
 
 const app = express();
--- server/db.js
+++ server/db.js
@@ -1,7 +1,24 @@
+import pgSession from "connect-pg-simple";
+import session, { MemoryStore } from "express-session";
 import { Pool } from "pg";
 
 import config from "./utils/config";
 import logger from "./utils/logger";
+
+/**
+ * Access this with `import db from "path/to/db";` then use it with
+ * `await db.query("<SQL>", [...<variables>])`.
+ */
+export default {
+	query: (...args) => {
+		if (!config.production) {
+			logger.debug("Postgres querying %O", args);
+		} else {
+			logger.debug("Postgres querying %s", args[0]);
+		}
+		return pool.query.apply(pool, args);
+	},
+};
 
 const pool = new Pool({
 	connectionString: config.dbUrl,
@@ -25,17 +42,14 @@ export const connectDb = async () => {
 
 export const disconnectDb = () => pool.end();
 
-/**
- * Access this with `import db from "path/to/db";` then use it with
- * `await db.query("<SQL>", [...<variables>])`.
- */
-export default {
-	query: (...args) => {
-		if (!config.production) {
-			logger.debug("Postgres querying %O", args);
-		} else {
-			logger.debug("Postgres querying %s", args[0]);
-		}
-		return pool.query.apply(pool, args);
-	},
-};
+export function getSessionStore() {
+	const store = config.sessionStore;
+	switch (store) {
+		case "memory":
+			return new MemoryStore();
+		case "postgres":
+			return new (pgSession(session))({ pool, tableName: "sessions" });
+		default:
+			throw new Error(`unknown store type: ${store}`);
+	}
+}
