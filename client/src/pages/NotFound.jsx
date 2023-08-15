770691ef641b4dcad49d6ffc4c16c5d935e04165
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-08-15 11:46:58 +0100

    Create example PR for review

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,7 +1,9 @@
 export default function NotFound() {
 	return (
 		<>
-			<h2>Not Found</h2>
+			<h2>
+				<span>Not Found</span>
+			</h2>
 			<p>Sorry, we couldn&apos;t find what you&apos;re looking for.</p>
 		</>
 	);
