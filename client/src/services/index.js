fcf5a5fecb1c6c4a452ecc8f72c29d20346b95d3
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-08-22 14:07:01 +0100

    Introduce service creation wrapper

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -5,6 +5,19 @@ import { useAuthenticatedFetch } from "../authContext";
 import ResourceService from "./resourceService";
 import TopicService from "./topicService";
 
+/**
+ * Inject the authenticated fetch wrapper into the specified service.
+ * @template T
+ * @param {{new (fetcher: typeof fetch): T}} Service
+ * @returns {T}
+ */
+export function useService(Service) {
+	const authenticatedFetch = useAuthenticatedFetch();
+	return useMemo(() => {
+		return new Service(authenticatedFetch);
+	}, [authenticatedFetch, Service]);
+}
+
 export function useResourceService() {
 	const authenticatedFetch = useAuthenticatedFetch();
 	return useMemo(
