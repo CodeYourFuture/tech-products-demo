6cd43254ad94a641164414729730e02c3fbebfbb
alinosratipour <alinosratipour@gmail.com>
2024-02-27 22:51:08 +0000

    wrote unit test for new feature filter resouce by topics

--- client/src/components/ResourceList/ResourceList.test.js
+++ client/src/components/ResourceList/ResourceList.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 
@@ -14,7 +14,9 @@ describe("ResourceList", () => {
 			title: "Data Binding in React",
 			url: "https://www.joshwcomeau.com/react/data-binding/",
 		});
-		render(<ResourceList resources={[resource]} pathname="/" />);
+		// const mytopics = resourceStub({ topic_name: "HTML/CSS", topic: "45678r448666" });
+
+		render(<ResourceList resources={[resource]} />);
 		expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
 			resource.title
 		);
@@ -25,7 +27,7 @@ describe("ResourceList", () => {
 	});
 
 	it("shows a message if no resources are available", () => {
-		render(<ResourceList resources={[]} pathname="/" />);
+		render(<ResourceList resources={[]} />);
 		expect(screen.getByText(/no resources to show/i)).toBeInTheDocument();
 	});
 
@@ -60,5 +62,61 @@ describe("ResourceList", () => {
 		const resource = resourceStub({ topic_name: "My Topic" });
 		render(<ResourceList resources={[resource]} />);
 		expect(screen.getByText(resource.topic_name)).toBeInTheDocument();
+	});
+
+	it("displays only resources with the selected topic", async () => {
+		const resource1 = resourceStub({
+			description:
+				'Comprehensive guide to setting up the various types of inputs with React (a.k.a. "data binding")',
+			title: "A Complete Guide to Flexbox",
+			url: "https://www.joshwcomeau.com/react/data-binding/",
+			topic: "84b099a4-8acd-4659-b5bd-1b89796fb924",
+			topic_name: "HTML/CSS",
+		});
+
+		const resource2 = resourceStub({
+			description: "Another resource with a different topic",
+			title: "Another Resource",
+			url: "https://example.com",
+			topic: "b3ab63b8-65f9-4f10-bac2-ce51ad1244c4",
+			topic_name: "Node & Express",
+		});
+
+		const resources = [resource1, resource2];
+
+		server.use(
+			rest.get("/api/topics", (req, res, ctx) =>
+				res(
+					ctx.json([
+						{ id: "84b099a4-8acd-4659-b5bd-1b89796fb924", name: "HTML/CSS" },
+						{
+							id: "b3ab63b8-65f9-4f10-bac2-ce51ad1244c4",
+							name: "Node & Express",
+						},
+					])
+				)
+			)
+		);
+
+		render(<ResourceList resources={resources} allowTopicFiltering={true} />);
+
+		// Wait for the select menu to be enabled and options to be populated
+		await waitFor(() => {
+			expect(
+				within(
+					screen.getByRole("combobox", { name: /filter topic/i })
+				).getByText("HTML/CSS")
+			).toBeInTheDocument();
+		});
+
+		// Select the desired topic
+		await userEvent.selectOptions(
+			screen.getByRole("combobox", { name: /filter topic/i }),
+			"HTML/CSS"
+		);
+
+		// Ensure that only resources with the selected topic are displayed
+		expect(screen.getByText(resource1.title)).toBeInTheDocument();
+		expect(screen.queryByText(resource2.title)).not.toBeInTheDocument();
 	});
 });
